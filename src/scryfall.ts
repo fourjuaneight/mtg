df264aecb05be890dde71005f4c943fc65dd78af
Juan Villela <juan@villela.co>
2022-07-16 22:58:31 -0400

    fix: Only filter scryfall data if set filter is present.

--- src/scryfall.ts
+++ src/scryfall.ts
@@ -33,7 +33,7 @@ export const searchCard = async (
         },
       }
     );
-    const response: ScryfallSearch | ScryfallError = await request.json(); 
+    const response: ScryfallSearch | ScryfallError = await request.json();
 
     if (response.warnings) {
       const { warnings } = response as ScryfallError;
@@ -41,56 +41,63 @@ export const searchCard = async (
       throw `(updateMTGItem): \n ${warnings.map(err => err).join('\n')}`;
     }
 
-    const cards = (response as ScryfallSearch).data
-      .map(
-        ({
-          artist,
-          collector_number,
-          colors,
-          flavor_text,
-          image_uris,
+    const cards = (response as ScryfallSearch).data.map(
+      ({
+        artist,
+        collector_number,
+        colors,
+        flavor_text,
+        image_uris,
+        name,
+        oracle_text,
+        rarity,
+        released_at,
+        set_name,
+        set,
+        type,
+      }) => {
+        const item: MTGItem = {
           name,
-          oracle_text,
-          rarity,
-          released_at,
-          set_name,
-          set,
+          colors: colors.map(color => MagicColor[color]),
           type,
-        }) => {
-          const item: MTGItem = {
-            name,
-            colors: colors.map(color => MagicColor[color]),
-            type,
-            set,
-            set_name,
-            oracle_text,
-            flavor_text: flavor_text || '',
-            rarity,
-            collector_number: parseInt(collector_number, 10),
-            artist,
-            released_at,
-            image: image_uris.png,
-          };
-
-          return item;
-        }
-      )
-      .filter(card => card.set === setMatch);
-      console.log(cards);
-      console.log(response);
-    // cards keyed by name, set, and collector number
-    const selection: ScryfallCardSelection[] = cards.reduce((acc, card) => {
-      const { name, set, collector_number } = card;
-      const key = `${name} - (${set}) #${collector_number}`;
-
-      if (!acc[key]) {
-        acc[key] = {
-          [key]: card,
+          set,
+          set_name,
+          oracle_text,
+          flavor_text: flavor_text || '',
+          rarity,
+          collector_number: parseInt(collector_number, 10),
+          artist,
+          released_at,
+          image: image_uris.png,
         };
-      }
 
-      return acc;
-    }, {} as ScryfallCardSelection);
+        return item;
+      }
+    );
+    const cleanCards = setMatch
+      ? cards.filter(card => card.set === setMatch?.toLocaleLowerCase())
+      : cards;
+    console.log(
+      cards.filter(card => card.set === setMatch?.toLocaleLowerCase()),
+      cards,
+      response
+    );
+    // cards keyed by name, set, and collector number
+    const selection: ScryfallCardSelection[] = cleanCards.reduce(
+      (acc, card) => {
+        const { name, set, collector_number } = card;
+        const key = `${name} - (${set}) #${collector_number}`;
+
+        if (!acc[key]) {
+          acc[key] = {
+            [key]: card,
+          };
+        }
+
+        return acc;
+      },
+      {} as ScryfallCardSelection
+    );
 
     return selection;
   } catch (error) {
